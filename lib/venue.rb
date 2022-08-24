124f91da6c0f8b22cc1e647be690962dad04cc5e
Will Wang <willy.j.wang@gmail,com>
2022-08-24 10:31:38 -0500

    Complete iteration 2 test 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,8 +1,13 @@
 class Venue
   attr_reader :name, :capacity, :patrons
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
   end
 end
